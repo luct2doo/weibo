f724f92ee5e66b33317ae7cd9bc5fce5f8047066
luct2doo <t2doo@qq.com>
2020-09-26 07:14:18 +0000

    增加 CSRF 验证

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,7 +8,9 @@
         <h5>注册</h5>
       </div>
       <div class="card-body">
-        <form method="POST" action="{{ route('users.store') }}">
+        <form action="{{ route('users.store') }}" method="POST">
+          @csrf
+
           <div class="form-group">
             <label for="name">名称：</label>
             <input type="text" name="name" class="form-control" value="{{ old('name') }}">
