827b02d8b886fc875ba8ab7bed305aa13af1833d
luct2doo <t2doo@qq.com>
2020-09-26 12:43:39 +0000

    查看用户列表

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class)->times(50)->create();
+
+        $user = User::find(1);
+        $user->name = 'Test';
+        $user->email = 'test@test.com';
+        $user->save();
+
+        $user = User::find(2);
+        $user->name = 'Monkey';
+        $user->email = 'monkey@test.com';
+        $user->save();
+    }
+}
